ce772d438c660829c23ebc145892008fdf1ccfce
nichm <nichmihai4@gmail.com>
2024-06-09 13:12:01 -0400

    Adding Model to the beer service

---
+++ src/main/java/nich/springframework/mssc_brewery_service/web/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package nich.springframework.mssc_brewery_service.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
