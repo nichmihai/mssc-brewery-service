3aeaaa63f79056f075a8258e88f842ad123ea758
nichm <nichmihai4@gmail.com>
2024-06-09 21:47:24 -0400

    Adding the Controller for Beer Service

--- src/main/java/nich/springframework/mssc_brewery_service/web/controller/BeerController.java
+++ src/main/java/nich/springframework/mssc_brewery_service/web/controller/BeerController.java
@@ -1,9 +1,33 @@
 package nich.springframework.mssc_brewery_service.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import nich.springframework.mssc_brewery_service.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+        //todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable UUID beerId ,@RequestBody BeerDto beerDto){
+        //todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    
 }
