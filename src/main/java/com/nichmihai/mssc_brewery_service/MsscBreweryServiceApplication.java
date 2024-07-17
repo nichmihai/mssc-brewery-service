8543f55e65ac4b8c3a4365445d5a3f8ca1ed3bf0
nichm <nichmihai4@gmail.com>
2024-07-16 22:20:01 -0400

    Add first commit

---
+++ src/main/java/com/nichmihai/mssc_brewery_service/MsscBreweryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nichmihai.mssc_brewery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryServiceApplication.class, args);
+	}
+
+}
